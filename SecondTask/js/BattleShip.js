249d528f197133aa6545634d5e1d30fe19a23da1
Jaime Barriga <jhbk.jbc@gmail.com>
2014-07-18 00:54:32 -0400

    some updates in the game field.

--- SecondTask/js/BattleShip.js
+++ SecondTask/js/BattleShip.js
@@ -6,22 +6,24 @@ console.log('This is the Battle ship game .....');
 var gameField = [];
 
 var createGameField = function(size) {
-    //gameField = [['X1','X2','X3'],['Y1','Y2','Y3'],['Z1','Z2','Z3']];
+    console.log('Creating a game field of : '+size);
 
     for(i=0; i<size; i++) {
+        gameField[i] = [];
         for(j=0; j<size; j++) {
             gameField[i][j]=' X ';
         }
     }
 
     drawGameField();
-    drawDebbugGameField();
+    //drawDebbugGameField();
 };
 
 var drawGameField = function() {
     console.log('Drawing the game field .... ');
     for(i=0; i<gameField.length; i++) {
         console.log(gameField[i].join());
+        console.log('\n');
     }
 };
 
@@ -29,6 +31,11 @@ var drawDebbugGameField = function() {
     console.warn('Drawing the game field .... ');
     for(i=0; i<gameField.length; i++){
         console.warn(gameField[i].join());
+        console.warn('\n');
     }
+};
+
+var addBattleShips = function(numberOfShips) {
+
 };
 
